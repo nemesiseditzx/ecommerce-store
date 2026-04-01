e46d7b0895a1c9ede7694ea322d19085880a1d5c
nemesiseditzx <nemesiseditzx984@gmail.com>
2026-03-31 21:19:46 -0400

    Update main.py

--- ecommerce-store/backend/main.py
+++ ecommerce-store/backend/main.py
@@ -23,3 +23,13 @@ def get_products():
         {"id": 2, "name": "Perfume B", "price": 70},
         {"id": 3, "name": "Perfume C", "price": 90}
     ]
+
+orders = []
+
+@app.post("/orders")
+def create_order(order: dict):
+    order_id = len(orders) + 1
+    order["id"] = order_id
+    orders.append(order)
+
+    return {"order_id": order_id}
